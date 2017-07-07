e9e770777765a905b02d3aba275d26c0a26f8ae0
Peter Hospodka <phospodka@gmail.com>
2017-07-06 23:48:42 -0500

    Added some tests for the Curator class

--- src/test/net/resonanceb/curator/CuratorTest.java
+++ src/test/net/resonanceb/curator/CuratorTest.java
@@ -3,6 +3,9 @@ package net.resonanceb.curator;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -21,6 +24,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+
 @RunWith(MockitoJUnitRunner.class)
 public class CuratorTest {
 
@@ -37,17 +42,21 @@ public class CuratorTest {
     private Client client;
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
         when(elasticClient.getClient()).thenReturn(client);
+        when(operations.findAllIndices(any(IndexOptions.class), any(Client.class))).thenReturn(Arrays.asList("index1", "index2"));
     }
 
     @Test
     public void testClose() throws Exception {
         ArgumentCaptor<IndexOptions> indexOptionsCaptor = ArgumentCaptor.forClass(IndexOptions.class);
+
         curator.close();
+
         verify(operations).findAllIndices(indexOptionsCaptor.capture(), any(Client.class));
-        //verify(operations).closeIndex(anyString(), any(Client.class));
+        verify(operations, times(2)).closeIndex(anyString(), any(Client.class));
+
         IndexOptions indexOptions = indexOptionsCaptor.getValue();
         assertTrue(indexOptions.isIncludeOpen());
         assertFalse(indexOptions.isIncludeClosed());
@@ -55,16 +64,43 @@ public class CuratorTest {
 
     @Test
     public void testDelete() throws Exception {
+        ArgumentCaptor<IndexOptions> indexOptionsCaptor = ArgumentCaptor.forClass(IndexOptions.class);
 
+        curator.delete();
+
+        verify(operations).findAllIndices(indexOptionsCaptor.capture(), any(Client.class));
+        verify(operations, times(2)).deleteIndex(anyString(), any(Client.class));
+
+        IndexOptions indexOptions = indexOptionsCaptor.getValue();
+        assertTrue(indexOptions.isIncludeOpen());
+        assertTrue(indexOptions.isIncludeClosed());
     }
 
     @Test
     public void testBackup() throws Exception {
+        ArgumentCaptor<IndexOptions> indexOptionsCaptor = ArgumentCaptor.forClass(IndexOptions.class);
 
+        curator.backup();
+
+        verify(operations).findAllIndices(indexOptionsCaptor.capture(), any(Client.class));
+        verify(operations, times(2)).createSnapshot(anyString(), any(Client.class));
+
+        IndexOptions indexOptions = indexOptionsCaptor.getValue();
+        assertTrue(indexOptions.isIncludeOpen());
+        assertFalse(indexOptions.isIncludeClosed());
     }
 
     @Test
     public void testForceMerge() throws Exception {
+        ArgumentCaptor<IndexOptions> indexOptionsCaptor = ArgumentCaptor.forClass(IndexOptions.class);
 
+        curator.forceMerge();
+
+        verify(operations).findAllIndices(indexOptionsCaptor.capture(), any(Client.class));
+        verify(operations, times(2)).forceMergeIndex(anyString(), anyInt(), any(Client.class));
+
+        IndexOptions indexOptions = indexOptionsCaptor.getValue();
+        assertTrue(indexOptions.isIncludeOpen());
+        assertFalse(indexOptions.isIncludeClosed());
     }
 }
